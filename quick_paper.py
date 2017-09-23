9884fbeed98a1de67adfa867bad53b23878172c3
Philip Trauner <philip.trauner@aol.com>
2017-09-23 16:50:59 +0200

    Improved quick_paper, now detects changes in images

--- quick_paper.py
+++ quick_paper.py
@@ -1,34 +1,104 @@
 from distutils.spawn import find_executable
-from subprocess import check_call
+from subprocess import call
 from os.path import isdir, isfile
 from os import mkdir, listdir
+from hashlib import md5 as md5_
+from json import load, dump
+from mimetypes import guess_type
 
 PAPER = "paper.tex"
 QUICK_PAPER = "quick_paper.tex"
-IMAGES_LOCATION = "images"
-RESIZED_IMAGES_LOCATION = "images_resized"
-IMAGEMAGICK_INSTALLED = True if find_executable("convert") else False
+IMAGES_LOCATION = "images/"
+RESIZED_IMAGES_LOCATION = "images_resized/"
+RESIZED_IMAGES_HASHES = RESIZED_IMAGES_LOCATION + "hashes.json"
+IMAGEMAGICK_INSTALLED = True if find_executable("mogrify") else False
 
-out = ""
+QUALITY = 20
+RESIZE_PERCENTAGE = 30
+
+def md5(fname):
+	""" 
+	https://stackoverflow.com/a/3431838/4739690
+
+	md5 is percise enough for this use-case, this isn't crypto after all ;)
+	"""		
+	hash_md5 = md5_()
+	with open(fname, "rb") as f:
+		for chunk in iter(lambda: f.read(4096), b""):
+			hash_md5.update(chunk)
+	return hash_md5.hexdigest()
+
+
+def reduce_image_quality(src, dest):
+	""" 
+	Reduce image quality to speed up latex rendering
+	"""	
+	return call([
+		"mogrify", "-quality", str(QUALITY), 
+		"-resize", str(RESIZE_PERCENTAGE) + "%", "-path", dest, src
+	])
+
+
+def patch_graphicspath(src, graphics_path, dest):
+	""" 
+	Patch graphicspath to point to resized images location 
+	"""
+	tmp = ""
+
+	for line in open(src, "r"):
+		if line.startswith("\graphicspath{"):
+			tmp += "\graphicspath{{%s/}}\n" % graphics_path
+		else:
+			tmp += line	
+	open(dest, "w").write(tmp)
+
+
+def is_image(path):
+	"""
+	Try to guess if file is an images based upon it's mimetype
+	"""
+	type_ = guess_type(path)[0]
+	if type_ != None:
+		return type_.startswith("image/")
+	return False
+
+
+def dump_image_hash_index(folder, dest):
+	hashes = {}
+
+	folder = folder + "/" if not folder.endswith("/") else folder
+
+	for file in listdir(folder):
+		if is_image(file):
+			hashes[file] = md5(folder + file)	
+
+	dump(hashes, open(dest, "w"))
+
+
 
 if IMAGEMAGICK_INSTALLED:
 	if not isdir(RESIZED_IMAGES_LOCATION):
 		mkdir(RESIZED_IMAGES_LOCATION)
+	if not isfile(RESIZED_IMAGES_HASHES):
+		dump_image_hash_index(IMAGES_LOCATION, RESIZED_IMAGES_HASHES)
 
-	for image in listdir(IMAGES_LOCATION):
-		if not isfile("{}/{}".format(RESIZED_IMAGES_LOCATION, image)):
-			# Not patient enough to convert command to list
-			if image.lower().endswith((".jpeg", ".png", ".jpg")):
-				check_call("mogrify -quality 20 -resize 30% -path {} {}/{}".format(
-					RESIZED_IMAGES_LOCATION, IMAGES_LOCATION, image), shell=True)
+	hashes = load(open(RESIZED_IMAGES_HASHES, "r"))
 
-	for line in open(PAPER, "r"):
-		if line.startswith("\graphicspath{"):
-			out += "\graphicspath{{%s/}}\n" % RESIZED_IMAGES_LOCATION
-		else:
-			out += line
+	for file in listdir(IMAGES_LOCATION):
+		if is_image(IMAGES_LOCATION + file):
+			if not isfile(RESIZED_IMAGES_LOCATION + file):
+				print("New image: '%s'" % file)
+				reduce_image_quality(IMAGES_LOCATION + file, 
+					RESIZED_IMAGES_LOCATION)
+			else:
+				if hashes[file] != md5(IMAGES_LOCATION + file):
+					print("Changes in image: '%s'" % file)
+					reduce_image_quality(IMAGES_LOCATION + file, 
+						RESIZED_IMAGES_LOCATION)
 
-	open(QUICK_PAPER, "w").write(out)
 
+	patch_graphicspath(PAPER, RESIZED_IMAGES_LOCATION, QUICK_PAPER)
+	dump_image_hash_index(IMAGES_LOCATION, RESIZED_IMAGES_HASHES)
 else:
 	print("ImageMagick not installed, can't resize.")
+	exit(1)
